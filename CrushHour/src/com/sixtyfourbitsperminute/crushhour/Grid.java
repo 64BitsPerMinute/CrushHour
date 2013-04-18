602a073e7720e494d23fc5e07c5794e7dc5b7b16
Jonathan Thompson <jmt4zj@virginia.edu>
2013-04-17 20:10:12 -0400

    Grid, Solver

--- CrushHour/src/com/sixtyfourbitsperminute/crushhour/Grid.java
+++ CrushHour/src/com/sixtyfourbitsperminute/crushhour/Grid.java
@@ -6,4 +6,54 @@ public class Grid {
 	ArrayList<Vehicle> vehicleList;
 	ArrayList<Grid> previousGrids;
 	
+	
+	public ArrayList<Vehicle> getVehicleList() {
+		return vehicleList;
+	}
+	public void setVehicleList(ArrayList<Vehicle> vehicleList) {
+		this.vehicleList = vehicleList;
+	}
+	public ArrayList<Grid> getPreviousGrids() {
+		return previousGrids;
+	}
+	public void setPreviousGrids(ArrayList<Grid> previousGrids) {
+		this.previousGrids = previousGrids;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((previousGrids == null) ? 0 : previousGrids.hashCode());
+		result = prime * result
+				+ ((vehicleList == null) ? 0 : vehicleList.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Grid other = (Grid) obj;
+		if (previousGrids == null) {
+			if (other.previousGrids != null)
+				return false;
+		} else if (!previousGrids.equals(other.previousGrids))
+			return false;
+		if (vehicleList == null) {
+			if (other.vehicleList != null)
+				return false;
+		} else if (!vehicleList.equals(other.vehicleList))
+			return false;
+		return true;
+	}
+	public Grid executeMove(Object object) {
+		
+		return null;
+	}
+	
 }
--- CrushHour/src/com/sixtyfourbitsperminute/crushhour/Solver.java
+++ CrushHour/src/com/sixtyfourbitsperminute/crushhour/Solver.java
@@ -1,8 +1,59 @@
 package com.sixtyfourbitsperminute.crushhour;
 
+import java.util.ArrayList;
+
 public class Solver {
 
 	public Grid bruteForce(Grid grid){
+		if(checkIfSolved(grid)){
+			return grid;
+		}
+		//for each vehicle
+		ArrayList<Vehicle> vehicles = grid.getVehicleList();
+		for(int i = 0; i < vehicles.size(); i++){
+		
+			//for each move
+			ArrayList moves = vehicles.get(i).getPossibleMoves(grid);
+			if(moves.isEmpty()){
+				return null;
+			}
+			
+			for(int j = 0; j < moves.size(); j++){
+				Grid g = bruteForce(grid.executeMove(moves.get(j)));
+				if(g!=null){
+					return grid;
+				}	
+			}
+			
+			return null;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		return grid;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private boolean checkIfSolved(Grid grid) {
+		return false;
+		
 	}
 }
