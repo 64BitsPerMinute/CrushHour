3a38e27b052b3525a8580fa4ddc3348f78a67c7f
Jonathan Thompson <jmt4zj@virginia.edu>
2013-04-17 20:15:04 -0400

    Cleaned Solver

--- CrushHour/src/com/sixtyfourbitsperminute/crushhour/Solver.java
+++ CrushHour/src/com/sixtyfourbitsperminute/crushhour/Solver.java
@@ -4,56 +4,36 @@ import java.util.ArrayList;
 
 public class Solver {
 
-	public Grid bruteForce(Grid grid){
-		if(checkIfSolved(grid)){
+	public Grid bruteForce(Grid grid) {
+		if (checkIfSolved(grid)) {
 			return grid;
 		}
-		//for each vehicle
+		
+		// for each vehicle
 		ArrayList<Vehicle> vehicles = grid.getVehicleList();
-		for(int i = 0; i < vehicles.size(); i++){
-		
-			//for each move
+		for (int i = 0; i < vehicles.size(); i++) {
+
+			// for each move
 			ArrayList moves = vehicles.get(i).getPossibleMoves(grid);
-			if(moves.isEmpty()){
-				return null;
+			if (moves.isEmpty()) {
+				continue;
 			}
-			
-			for(int j = 0; j < moves.size(); j++){
+
+			for (int j = 0; j < moves.size(); j++) {
 				Grid g = bruteForce(grid.executeMove(moves.get(j)));
-				if(g!=null){
+				if (g != null) {
 					return grid;
-				}	
+				}
 			}
-			
-			return null;
+
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		return grid;
+
+		return null;
+
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	private boolean checkIfSolved(Grid grid) {
 		return false;
-		
+
 	}
 }
