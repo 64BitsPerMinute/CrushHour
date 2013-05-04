e638a06c5996c04268391630e486d30c465ccac5
Kelly Croswell <kec6en@virginia.edu>
2013-05-04 15:14:50 -0400

    gridstrings comments

--- CrushHour/src/com/sixtyfourbitsperminute/crushhour/GridStrings.java
+++ CrushHour/src/com/sixtyfourbitsperminute/crushhour/GridStrings.java
@@ -2,7 +2,18 @@ package com.sixtyfourbitsperminute.crushhour;
 
 import java.util.Random;
 
+/**
+ * @author Jonathan Thompson
+ * @author Kelly Croswell
+ * 
+ * This class holds 50 potential puzzles that the GUI can interact with and that 
+ * the user will be able to choose to have the program solve. 
+ */
 public class GridStrings {
+	
+	/**
+	 * A String array containing all of the potential, premade grids.
+	 */
 	static String[] possibleGrids = {
 			"bbxxxc\ndxxexc\ndAAexc\ndxxexx\nfxxxgg\nfxhhhx\n",
 			"bxxccc\nbxxdxe\nAAxdfe\nfffxfe\nxxhxii\njjhkkx\n",
@@ -56,6 +67,10 @@ public class GridStrings {
 			"bbcxdd\nxxcxee\nAAfxxg\nhxfiig\nhjjkxl\nhmmkxl\n",
 	};
 	
+	/**
+	 * This method returns a random grid to be solved.
+	 * @return A string containing the grid.
+	 */
 	public static String getRandomGrid(){
 		Random rand = new Random();
 		int max = 50;
@@ -64,6 +79,12 @@ public class GridStrings {
 		return possibleGrids[randomNum];
 	}
 	
+	/**
+	 * This method returns a specific grid fro the array, so that users can choose 
+	 * the grid they want. 
+	 * @param i The index of the grid in the array.
+	 * @return A string containing the grid.
+	 */
 	public static String getGrid(int i){
 		return possibleGrids[i];
 	}
