4cf4dd5b472f3449c197424254fa45b071509233
Jonathan Thompson <jmt4zj@virginia.edu>
2013-05-05 21:23:48 -0400

    Added JavaOSC library

--- CrushHour/src/com/sixtyfourbitsperminute/crushhour/Main.java
+++ CrushHour/src/com/sixtyfourbitsperminute/crushhour/Main.java
@@ -1,5 +1,12 @@
 package com.sixtyfourbitsperminute.crushhour;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import com.illposed.osc.*;
+
 /**
  * @author Jonathan Thompson
  * @author Kelly Croswell
@@ -11,6 +18,8 @@ public class Main {
 	/**
 	 * @param args
 	 */
+	
+	
 	public static void main(String[] args) {
 		String gridString = GridStrings.getRandomGrid();
 		Parser parser = new Parser(gridString);
@@ -18,7 +27,25 @@ public class Main {
 			//Grid grid = parser.createGrid();
 			System.out.println("Created grid");
 		}
-
+		
+		//send test message
+		OSCPortOut sender = null;
+		try {
+			sender = new OSCPortOut();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		Object payload[] = new Object[1];
+		payload[0] = "bbxxxcexxdxceAAdxcexxdxxfxxxggfxhhhx";
+		OSCMessage mesg = new OSCMessage("/ch/grid", payload);
+			try {
+				sender.send(mesg);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 
 }
